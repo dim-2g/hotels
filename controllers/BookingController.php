1a00ce79b382cb53b2238d71951707921079c8ae
Дмитрий <dim-2g@yandex.ru>
2019-06-07 00:20:04 +0300

    рефакторинг

--- controllers/BookingController.php
+++ controllers/BookingController.php
@@ -3,12 +3,7 @@
 namespace app\controllers;
 
 use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\Response;
-use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
 use app\models\Booking;
 use app\helpers\BookingHelper;
 
